f6012d3d4ffb2020007bafe0340ce5fb5606ca8d
Ambrose Kibet <ambrosekibet36@gmail.com>
2023-06-04 22:47:45 +0300

    Update CheckoutForm.jsx

--- src/Components/CheckoutForm.jsx
+++ src/Components/CheckoutForm.jsx
@@ -58,7 +58,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: 'http://localhost:8888/success',
+        return_url: 'https://king-julien.netlify.app/success',
       },
     });
 
